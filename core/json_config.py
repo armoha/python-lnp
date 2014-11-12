b0a08e621cb53f10fdac0159205865551fe534bb
PeridexisErrant <PeridexisErrant@gmail.com>
2014-11-12 11:11:54 +1100

    ignore the testing data

---
+++ core/json_config.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Proxy to abstract access to JSON configuration and gracefully handle missing
+keys."""
+from __future__ import print_function, unicode_literals, absolute_import
+import sys, json
+
+if sys.version_info[0] == 3:  # Alternate import names
+    enc_dict = {}
+else:
+    enc_dict = {'encoding': 'utf-8'}
+
+class JSONConfiguration(object):
+    """Proxy for JSON-based configuration files."""
+
+    def __init__(self, filename):
+        """
+        Constructor for JSONConfiguration.
+
+        Params:
+            filename
+                JSON filename to load data from.
+        """
+        self.filename = filename
+        try:
+            self.data = json.load(open(filename), **enc_dict)
+        except:
+            print(
+                "Note: Failed to read JSON from " + filename +
+                ", ignoring data - error details follow", file=sys.stderr)
+            sys.excepthook(*sys.exc_info())
+            self.data = {}
+
+    def save_data(self):
+        """Saves the data to the JSON file."""
+        json.dump(
+            self.data, open(self.filename, 'w'), indent=2, **enc_dict)
+
+    def get_value(self, path, default=None):
+        """
+        Retrieves a value from the configuration.
+        Returns default if the path does not exist.
+
+        Params:
+            path
+                /-delimited path to the string.
+            default
+                Value returned if path does not exist.
+        """
+        try:
+            path = path.split('/')
+            result = self.data
+            for p in path:
+                result = result[p]
+            return result
+        except KeyError:
+            return default
+
+    def get_string(self, path):
+        """
+        Retrieves a value from the configuration.
+        Returns an empty string if the path does not exist.
+
+        Params:
+            path
+                /-delimited path to the string.
+        """
+        return self.get_value(path, "")
+
+    def get_number(self, path):
+        """
+        Retrieves a value from the configuration.
+        Returns 0 if the path does not exist.
+
+        Params:
+            path
+                /-delimited path to the string.
+        """
+        return self.get_value(path, 0)
+
+    def get_bool(self, path):
+        """
+        Retrieves a value from the configuration.
+        Returns False if the path does not exist.
+
+        Params:
+            path
+                /-delimited path to the string.
+        """
+        return self.get_value(path, False)
+
+    def get_list(self, path):
+        """
+        Retrieves a value from the configuration.
+        Returns an empty list if the path does not exist.
+
+        Params:
+            path
+                /-delimited path to the string.
+        """
+        return self.get_value(path, [])
+
+    def get_dict(self, path):
+        """
+        Retrieves a value from the configuration.
+        Returns an empty dictionary if the path does not exist.
+
+        Params:
+            path
+                /-delimited path to the string.
+        """
+        return self.get_value(path, {})
+
+    def set_value(self, key, value):
+        """
+        Writes a value to a key.
+        Note: Arbitrary paths not supported - you must refresh entire key.
+
+        Params:
+            key
+                The key to save the value under.
+            value
+                The value to save.
+        """
+        self.__setitem__(key, value)
+
+    def __getitem__(self, key):
+        """Accessor for indexing directly into the configuration."""
+        return self.get_value(key)
+
+    def __setitem__(self, key, value):
+        """Accessor for writing into the configuration with indexing."""
+        self.data[key] = value
---
+++ core/launcher.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Launching of programs, folders, URLs, etc.."""
+from __future__ import print_function, unicode_literals, absolute_import
+
+import sys
+import os
+import subprocess
+import distutils.spawn
+
+from .helpers import get_resource
+from .lnp import lnp
+from . import hacks, paths
+
+def toggle_autoclose():
+    """Toggle automatic closing of the UI when launching DF."""
+    lnp.userconfig['autoClose'] = not lnp.userconfig.get_bool('autoClose')
+    lnp.userconfig.save_data()
+
+def get_df_executable():
+    """Returns the path of the executable needed to launch Dwarf Fortress."""
+    base_path = paths.get('df')
+    spawn_terminal = False
+    if sys.platform == 'win32':
+        if ('legacy' in lnp.df_info.variations and
+                lnp.df_info.version <= '0.31.14'):
+            df_filename = 'dwarfort.exe'
+        else:
+            df_filename = 'Dwarf Fortress.exe'
+    elif sys.platform == 'darwin' and lnp.df_info.version <= '0.28.181.40d':
+        df_filename = 'Dwarf Fortress.app'
+    else:
+        # Linux/OSX: Run DFHack if available and enabled
+        if (os.path.isfile(os.path.join(base_path, 'dfhack')) and
+                hacks.is_dfhack_enabled()):
+            df_filename = 'dfhack'
+            spawn_terminal = True
+        else:
+            df_filename = 'df'
+
+    return df_filename, spawn_terminal
+
+def run_df(force=False):
+    """Launches Dwarf Fortress."""
+    df_filename, spawn_terminal = get_df_executable()
+    base_path = paths.get('df')
+
+    executable = os.path.join(base_path, df_filename)
+    result = run_program(executable, force, True, spawn_terminal)
+    if (force and not result) or result is False:
+        raise Exception('Failed to run Dwarf Fortress.')
+
+    util_path = paths.get('utilities')
+    for prog in lnp.autorun:
+        utility = os.path.join(util_path, prog)
+        if os.access(utility, os.F_OK):
+            run_program(utility)
+
+    if lnp.userconfig.get_bool('autoClose'):
+        sys.exit()
+    return result
+
+def get_terminal_launcher():
+    """Returns a command prefix to launch a program in a new terminal."""
+    if sys.platform == 'darwin':
+        return ['open', '-a', 'Terminal.app']
+    elif sys.platform.startswith('linux'):
+        # prefer distribution provided terminal launchers
+        if distutils.spawn.find_executable('x-terminal-emulator'):
+            return ['x-terminal-emulator', '-e']
+        if distutils.spawn.find_executable('xdg-terminal'):
+            return ['xdg-terminal']
+        return [get_resource('xdg-terminal')]
+    raise Exception('No terminal launcher for platform: ' + sys.platform)
+
+def run_program(path, force=False, is_df=False, spawn_terminal=False):
+    """
+    Launches an external program.
+
+    Params:
+        path
+            The path of the program to launch.
+        spawn_terminal
+            Whether or not to spawn a new terminal for this app.
+            Used only for DFHack.
+    """
+    path = os.path.abspath(path)
+    check_nonchild = ((spawn_terminal and sys.platform.startswith('linux')) or
+                      (sys.platform == 'darwin' and (
+                          path.endswith('.app') or spawn_terminal)))
+
+    is_running = program_is_running(path, check_nonchild)
+    if not force and is_running:
+        lnp.ui.on_program_running(path, is_df)
+        return None
+
+    try:
+        workdir = os.path.dirname(path)
+        run_args = path
+        if spawn_terminal and not sys.platform.startswith('win'):
+            term = get_terminal_launcher()
+            retcode = subprocess.call(term + [path], cwd=workdir)
+            return retcode == 0
+        elif path.endswith('.jar'):  # Explicitly launch JAR files with Java
+            run_args = ['java', '-jar', os.path.basename(path)]
+        elif path.endswith('.app'):  # OS X application bundle
+            run_args = ['open', path]
+            workdir = path
+
+        lnp.running[path] = subprocess.Popen(run_args, cwd=workdir)
+        return True
+    except OSError:
+        sys.excepthook(*sys.exc_info())
+        return False
+
+def program_is_running(path, nonchild=False):
+    """
+    Returns True if a program is currently running.
+
+    Params:
+        path
+            The path of the program.
+        nonchild
+            If set to True, attempts to check for the process among all
+            running processes, not just known child processes. Used for
+            DFHack on Linux and OS X; currently unsupported for Windows.
+    """
+    if nonchild:
+        ps = subprocess.Popen(['ps', 'axww'], stdout=subprocess.PIPE)
+        s = ps.stdout.read()
+        ps.wait()
+        return path in s
+    else:
+        if path not in lnp.running:
+            return False
+        else:
+            lnp.running[path].poll()
+            return lnp.running[path].returncode is None
+
+def open_folder_idx(i):
+    """Opens the folder specified by index i, as listed in PyLNP.json."""
+    open_folder(os.path.join(
+        paths.get('root'), lnp.config['folders'][i][1].replace(
+            '<df>', paths.get('df'))))
+
+def open_savegames():
+    """Opens the save game folder."""
+    open_folder(paths.get('save'))
+
+def open_link_idx(i):
+    """Opens the link specified by index i, as listed in PyLNP.json."""
+    open_url(lnp.config['links'][i][1])
+
+def open_url(url):
+    """Launches a web browser to the Dwarf Fortress webpage."""
+    import webbrowser
+    webbrowser.open(url)
+
+def open_folder(path):
+    """
+    Opens a folder in the system file manager.
+
+    Params:
+        path
+            The folder path to open.
+    """
+    # http://stackoverflow.com/q/6631299
+    path = os.path.normpath(path)
+    # pylint: disable=broad-except, bare-except
+    try:
+        if sys.platform == 'darwin':
+            subprocess.check_call(['open', '--', path])
+        elif sys.platform.startswith('linux'):
+            subprocess.check_call(['xdg-open', path])
+        elif sys.platform in ['windows', 'win32']:
+            subprocess.check_call(['explorer', path])
+    except:
+        pass
