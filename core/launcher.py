cd31a659d69eef41468870da9a7ed205efd0c8b2
Peridexis Errant <PeridexisErrant@gmail.com>
2015-02-05 10:26:14 +1100

    Instead of giving selected items in listboxes red text, use a pale green background.
    Several users complained to me that the red text was confusing, associated with an error rather than a selection.  A pale green background should avoid that concern.

---
+++ core/launcher.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Launching of programs, folders, URLs, etc.."""
+from __future__ import print_function, unicode_literals, absolute_import
+
+import sys
+import os
+import subprocess
+
+from .helpers import get_resource
+from .lnp import lnp
+from . import hacks, paths
+
+def toggle_autoclose():
+    """Toggle automatic closing of the UI when launching DF."""
+    lnp.userconfig['autoClose'] = not lnp.userconfig.get_bool('autoClose')
+    lnp.userconfig.save_data()
+
+def get_df_executable():
+    """Returns the path of the executable needed to launch Dwarf Fortress."""
+    spawn_terminal = False
+    if sys.platform == 'win32':
+        if ('legacy' in lnp.df_info.variations and
+                lnp.df_info.version <= '0.31.14'):
+            df_filename = 'dwarfort.exe'
+        else:
+            df_filename = 'Dwarf Fortress.exe'
+    elif sys.platform == 'darwin' and lnp.df_info.version <= '0.28.181.40d':
+        df_filename = 'Dwarf Fortress.app'
+    else:
+        # Linux/OSX: Run DFHack if available and enabled
+        if (os.path.isfile(paths.get('df', 'dfhack')) and
+                hacks.is_dfhack_enabled()):
+            df_filename = 'dfhack'
+            spawn_terminal = True
+        else:
+            df_filename = 'df'
+
+    return df_filename, spawn_terminal
+
+def run_df(force=False):
+    """Launches Dwarf Fortress."""
+    df_filename, spawn_terminal = get_df_executable()
+
+    executable = paths.get('df', df_filename)
+    result = run_program(executable, force, True, spawn_terminal)
+    if (force and not result) or result is False:
+        raise Exception('Failed to run Dwarf Fortress.')
+
+    for prog in lnp.autorun:
+        utility = paths.get('utilities', prog)
+        if os.access(utility, os.F_OK):
+            run_program(utility)
+
+    if lnp.userconfig.get_bool('autoClose'):
+        sys.exit()
+    return result
+
+def get_terminal_launcher():
+    """Returns a command prefix to launch a program in a new terminal."""
+    if sys.platform == 'darwin':
+        return ['open', '-a', 'Terminal.app']
+    elif sys.platform.startswith('linux'):
+        return [get_resource('xdg-terminal')]
+    raise Exception('No terminal launcher for platform: ' + sys.platform)
+
+def run_program(path, force=False, is_df=False, spawn_terminal=False):
+    """
+    Launches an external program.
+
+    Params:
+        path
+            The path of the program to launch.
+        spawn_terminal
+            Whether or not to spawn a new terminal for this app.
+            Used only for DFHack.
+    """
+    path = os.path.abspath(path)
+    check_nonchild = ((spawn_terminal and sys.platform.startswith('linux')) or
+                      (sys.platform == 'darwin' and (
+                          path.endswith('.app') or spawn_terminal)))
+
+    is_running = program_is_running(path, check_nonchild)
+    if not force and is_running:
+        lnp.ui.on_program_running(path, is_df)
+        return None
+
+    try:
+        workdir = os.path.dirname(path)
+        run_args = path
+        if spawn_terminal and not sys.platform.startswith('win'):
+            term = get_terminal_launcher()
+            retcode = subprocess.call(term + [path], cwd=workdir)
+            return retcode == 0
+        elif path.endswith('.jar'):  # Explicitly launch JAR files with Java
+            run_args = ['java', '-jar', os.path.basename(path)]
+        elif path.endswith('.app'):  # OS X application bundle
+            run_args = ['open', path]
+            workdir = path
+
+        lnp.running[path] = subprocess.Popen(run_args, cwd=workdir)
+        return True
+    except OSError:
+        sys.excepthook(*sys.exc_info())
+        return False
+
+def program_is_running(path, nonchild=False):
+    """
+    Returns True if a program is currently running.
+
+    Params:
+        path
+            The path of the program.
+        nonchild
+            If set to True, attempts to check for the process among all
+            running processes, not just known child processes. Used for
+            DFHack on Linux and OS X; currently unsupported for Windows.
+    """
+    if nonchild:
+        ps = subprocess.Popen(['ps', 'axww'], stdout=subprocess.PIPE)
+        s = ps.stdout.read()
+        ps.wait()
+        return path in s.decode(sys.getfilesystemencoding())
+    else:
+        if path not in lnp.running:
+            return False
+        else:
+            lnp.running[path].poll()
+            return lnp.running[path].returncode is None
+
+def open_folder_idx(i):
+    """Opens the folder specified by index i, as listed in PyLNP.json."""
+    open_folder(os.path.join(
+        paths.get('root'), lnp.config['folders'][i][1].replace(
+            '<df>', paths.get('df'))))
+
+def open_savegames():
+    """Opens the save game folder."""
+    open_folder(paths.get('save'))
+
+def open_link_idx(i):
+    """Opens the link specified by index i, as listed in PyLNP.json."""
+    open_url(lnp.config['links'][i][1])
+
+def open_url(url):
+    """Launches a web browser to the Dwarf Fortress webpage."""
+    import webbrowser
+    webbrowser.open(url)
+
+def open_folder(path):
+    """
+    Opens a folder in the system file manager.
+
+    Params:
+        path
+            The folder path to open.
+    """
+    # http://stackoverflow.com/q/6631299
+    path = os.path.normpath(path)
+    # pylint: disable=broad-except, bare-except
+    try:
+        if sys.platform == 'darwin':
+            subprocess.check_call(['open', '--', path])
+        elif sys.platform.startswith('linux'):
+            subprocess.check_call(['xdg-open', path])
+        elif sys.platform in ['windows', 'win32']:
+            subprocess.check_call(['explorer', path])
+    except:
+        pass
---
+++ tkgui/utilities.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# pylint:disable=unused-wildcard-import,wildcard-import,invalid-name,attribute-defined-outside-init
+"""Utilities tab for the TKinter GUI."""
+from __future__ import print_function, unicode_literals, absolute_import
+
+from . import controls
+from .tab import Tab
+import sys, os
+
+from core import launcher, paths, utilities
+from core.lnp import lnp
+
+if sys.version_info[0] == 3:  # Alternate import names
+    # pylint:disable=import-error
+    from tkinter import *
+    from tkinter.ttk import *
+else:
+    # pylint:disable=import-error
+    from Tkinter import *
+    from ttk import *
+
+class UtilitiesTab(Tab):
+    """Utilities tab for the TKinter GUI."""
+    def create_variables(self):
+        self.progs = Variable()
+
+    def read_data(self):
+        self.read_utilities()
+
+    def create_controls(self):
+        progs = controls.create_control_group(
+            self, 'Programs/Utilities', True)
+        progs.pack(side=TOP, expand=Y, fill=BOTH)
+        Grid.rowconfigure(progs, 3, weight=1)
+
+        controls.create_trigger_button(
+            progs, 'Run Program', 'Runs the selected program(s).',
+            self.run_selected_utilities).grid(column=0, row=0, sticky="nsew")
+        controls.create_trigger_button(
+            progs, 'Open Utilities Folder', 'Open the utilities folder',
+            utilities.open_utils).grid(column=1, row=0, sticky="nsew")
+        Label(
+            progs, text='Double-click on a program to launch it.').grid(
+                column=0, row=1, columnspan=2)
+        Label(
+            progs, text='Right-click on a program to toggle auto-launch.').grid(
+                column=0, row=2, columnspan=2)
+
+        self.proglist = proglist = controls.create_toggle_list(
+            progs, ('exe', 'launch'),
+            {'column': 0, 'row': 3, 'columnspan': 2, 'sticky': "nsew"})
+        proglist.column('exe', width=1, anchor='w')
+        proglist.column('launch', width=35, anchor='e', stretch=NO)
+        proglist.heading('exe', text='Executable')
+        proglist.heading('launch', text='Auto')
+        proglist.bind("<Double-1>", lambda e: self.run_selected_utilities())
+        if sys.platform == 'darwin':
+            proglist.bind("<2>", self.toggle_autorun)
+        else:
+            proglist.bind("<3>", self.toggle_autorun)
+
+        self.list_tooltip = controls.create_tooltip(proglist, '')
+        proglist.bind('<Motion>', self.update_tooltip)
+
+        refresh = controls.create_trigger_button(
+            progs, 'Refresh List', 'Refresh the list of utilities',
+            self.read_utilities)
+        refresh.grid(column=0, row=4, columnspan=2, sticky="nsew")
+
+    def update_tooltip(self, event):
+        """
+        Event handler for mouse motion over the utility list.
+        Used to update the tooltip.
+        """
+        tip = utilities.get_tooltip(self.proglist.item(self.proglist.identify(
+            'row', event.x, event.y))['text'])
+        self.list_tooltip.settext(tip)
+
+    def read_utilities(self):
+        """Reads list of utilities."""
+        self.progs = utilities.read_utilities()
+        self.update_autorun_list()
+
+    def toggle_autorun(self, event):
+        """
+        Toggles autorun for a utility.
+
+        Params:
+            event
+                Data for the click event that triggered this.
+        """
+        utilities.toggle_autorun(self.proglist.item(self.proglist.identify(
+            'row', event.x, event.y), 'text'))
+        self.update_autorun_list()
+
+    def update_autorun_list(self):
+        """Updates the autorun list."""
+        for i in self.proglist.get_children():
+            self.proglist.delete(i)
+        for p in self.progs:
+            title = utilities.get_title(p)
+            self.proglist.insert('', 'end', text=p, values=(
+                title, 'Yes' if p in lnp.autorun else 'No'))
+
+    def run_selected_utilities(self):
+        """Runs selected utilities."""
+        for item in self.proglist.selection():
+            utility_path = self.proglist.item(item, 'text')
+            launcher.run_program(paths.get('utilities', utility_path))
+
