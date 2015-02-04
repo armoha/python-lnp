33a0f46fbf8e4e7e5523a6e219abc7a879f342c2
Michael Madsen <michael@birdiesoft.dk>
2015-02-05 00:40:21 +0100

    xdg-terminal: Improve xfce detection

---
+++ core/baselines.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Advanced raw and data folder management, for mods or graphics packs."""
+from __future__ import print_function, unicode_literals, absolute_import
+
+import os, filecmp, glob, zipfile, fnmatch
+
+from . import paths, update
+from .lnp import lnp
+
+def find_vanilla(download_missing=True):
+    """Finds the vanilla baseline for the current version.
+
+    Starts by unzipping any DF releases in baselines and preprocessing them.
+    If download_missing is set to True, missing baselines will be downloaded.
+
+    Returns:
+        Path to the vanilla folder, eg 'LNP/Baselines/df_40_15'
+        False if baseline not available (and start download)
+        None if version detection is not accurate
+    """
+    if lnp.df_info.source == "init detection":
+        # WARNING: probably the wrong version!  Restore 'release notes.txt'.
+        return None
+    prepare_baselines()
+    version = 'df_' + str(lnp.df_info.version)[2:].replace('.', '_')
+    if os.path.isdir(paths.get('baselines', version)):
+        return paths.get('baselines', version)
+    if download_missing:
+        update.download_df_baseline()
+    return False
+
+def find_vanilla_raws(download_missing=True):
+    """Finds vanilla raws for the current version."""
+    retval = find_vanilla(download_missing)
+    if retval:
+        return os.path.join(retval, 'raw')
+    return retval
+
+def prepare_baselines():
+    """Unzip any DF releases found, and discard non-universial files."""
+    zipped = glob.glob(os.path.join(paths.get('baselines'), 'df_??_?*.zip'))
+    for item in zipped:
+        version = os.path.basename(item)
+        for s in ['_win', '_legacy', '_s', '.zip']:
+            version = version.replace(s, '')
+        f = paths.get('baselines', version)
+        if not os.path.isdir(f):
+            zipfile.ZipFile(item).extractall(f)
+            simplify_pack(version, 'baselines')
+        os.remove(item)
+
+def set_auto_download(value):
+    """Sets the option for auto-download of baselines."""
+    lnp.userconfig['downloadBaselines'] = value
+    lnp.userconfig.save_data()
+
+def simplify_pack(pack, folder):
+    """Removes unnecessary files from LNP/<folder>/<pack>.
+
+    Params:
+        pack, folder
+            path segments in './LNP/folder/pack/' as strings
+
+    Returns:
+        The number of files removed if successful
+        False if an exception occurred
+        None if folder is empty
+    """
+    valid_dirs = ('graphics', 'mods', 'baselines')
+    if not folder in valid_dirs:
+        return False
+    files_before = sum(len(f) for (_, _, f) in os.walk(paths.get(folder, pack)))
+    if files_before == 0:
+        return None
+    keep = [os.path.join('raw', 'graphics', '*')]
+    if not folder == 'graphics':
+        keep += [os.path.join('raw', 'objects', '*'),
+                 os.path.join('data', 'speech', '*')]
+    if not folder == 'mods':
+        keep += [os.path.join('data', 'art', '*'),
+                 os.path.join('data', 'init' '*')]
+    for root, _, files in os.walk(paths.get(folder, pack)):
+        d = paths.get(folder, pack)
+        for k in files:
+            f = os.path.join(root, k)
+            if not any(fnmatch.fnmatch(f, os.path.join(d, p)) for p in keep):
+                os.remove(f)
+    if not folder == 'mods':
+        init_files = ('colors', 'd_init', 'init', 'overrides')
+        init_dir = paths.join(folder, pack, 'data', 'init')
+        for f in os.listdir(init_dir):
+            if not any(p in f for p in init_files):
+                os.remove(os.path.join(init_dir, f))
+    files_after = sum(len(f) for (_, _, f) in os.walk(paths.get(folder, pack)))
+    return files_after - files_before
+
+def remove_vanilla_raws_from_pack(pack, folder):
+    """Remove files identical to vanilla raws, return files removed
+
+    Params:
+        pack, folder
+            path segments in './LNP/folder/pack/' as strings
+
+    Returns:
+        The number of files removed
+    """
+    files_before = sum(len(f) for (_, _, f) in os.walk(paths.get(folder, pack)))
+    for folder, van_folder in (
+            [paths.get(folder, pack, 'raw'), find_vanilla_raws()],
+            [paths.get(folder, pack, 'data', 'speech'),
+             paths.get('baselines', pack, 'data', 'speech')]):
+        for root, _, files in os.walk(folder):
+            for k in files:
+                f = os.path.join(root, k)
+                silently_kill = ('Thumbs.db', 'installed_raws.txt')
+                if any(f.endswith(x) for x in silently_kill):
+                    os.remove(f)
+                    continue
+                van_f = os.path.join(van_folder, os.path.relpath(f, folder))
+                if os.path.isfile(van_f):
+                    if filecmp.cmp(f, van_f):
+                        os.remove(f)
+    files_after = sum(len(f) for (_, _, f) in os.walk(paths.get(folder, pack)))
+    return files_after - files_before
+
+def remove_empty_dirs(pack, folder):
+    """Removes empty subdirs in a mods or graphics pack.
+
+    Params:
+        pack, folder
+            path segments in './LNP/folder/pack/' as strings
+
+    Returns:
+        The number of dirs removed
+    """
+    i = 0
+    for _ in range(3):
+        # only catches the lowest level each iteration
+        for root, dirs, files in os.walk(paths.get(folder, pack)):
+            if not dirs and not files:
+                os.rmdir(root)
+                i += 1
+    return i
---
+++ core/dfraw.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Code used for modifying DF RAW files"""
+from __future__ import print_function, unicode_literals, absolute_import
+import io
+import re
+import sys
+
+if sys.version_info[0] == 3:
+    basestring = str
+
+class DFRaw(object):
+    _option_disable = ("[{0}]", "!{0}!")
+    _option_enable = tuple(reversed(_option_disable))
+
+    def __init__(self, path):
+        self.path = path
+        self.modified = False
+        self.text = self.read(path)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.modified:
+            self.save()
+
+    @staticmethod
+    def open(path, mode):
+        """
+        Opens a raw file at <path> with mode <mode> and returns a stream
+        :param str path: Path to raw file
+        :param mode: File mode
+        :return io.TextIOWrapper:
+        """
+        return io.open(path, mode, encoding='cp437', errors='replace')
+
+    @classmethod
+    def read(cls, path):
+        """
+        Returns text of raw file at <path>
+        :param str path:
+        :return str:
+        """
+        with cls.open(path, 'rt') as fd:
+            return fd.read()
+
+    @classmethod
+    def write(cls, path, text):
+        """
+        Writes <text> to a raw file at <path>
+        :param str path:
+        :param str text:
+        :return:
+        """
+        with cls.open(path, 'wt') as fd:
+            return fd.write(text)
+
+    def save(self):
+        """
+        Writes current raw file
+        :return int: bytes written
+        """
+        return self.write(self.path, self.text)
+
+    def toggle(self, field, enabled):
+        """
+        Toggles (disables/enables) a <field> in the raw file
+        :param str field: Name of the field
+        :param bool enabled:
+        :return:
+        """
+        self.modified = True
+        if enabled:
+            replace_from, replace_to = self._option_enable
+        else:
+            replace_from, replace_to = self._option_disable
+
+        self.text = self.text.replace(
+            replace_from.format(field),
+            replace_to.format(field))
+
+    def set_value(self, field, value):
+        """
+        Sets the value of <field> to <value>
+        :param str field:
+        :param str value:
+        :return:
+        """
+        self.modified = True
+        self.text = re.sub(
+            r'\[{0}:(.+?)\]'.format(field),
+            '[{0}:{1}]'.format(
+                field, value), self.text)
+
+    def get_value(self, field):
+        """
+        Returns value of field <field>. If multiple fields with this name exist,
+        returns the first one.
+        If no such field exists returns None.
+
+        :param str field: The field to read
+        :return str or None: The value of the field or None
+        """
+        match = re.search(r'\['+str(field)+r':(.+?)\]', self.text)
+        if match:
+            return match.group(1)
+        return None
+
+    def get_values(self, *fields):
+        """
+        Return values of <fields>. The nesting and order of the
+        resulting list will match the nesting and order of <fields>
+        For any field the behaviour is the same as get_field.
+        :param fields:
+        :return list:
+        """
+        result = []
+        for field in fields:
+            if isinstance(field, (str, basestring)):
+                result.append(self.get_value(field))
+            elif isinstance(field, (tuple, list)):
+                result.append(self.get_values(*field))
+            else:
+                result.append(None)
+        return result
