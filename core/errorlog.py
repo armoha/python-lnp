fe2a1ef1f346f070be9ec9d20c58ac9c94ec7b88
Michael Madsen <michael@birdiesoft.dk>
2015-02-07 15:27:49 +0100

    Catch components >= 256 when painting color scheme preview

---
+++ core/errorlog.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Framework for logging errors."""
+from __future__ import print_function, unicode_literals, absolute_import
+import sys
+from io import open
+
+from . import paths
+
+class CaptureStream(object):
+    """ Redirects output to a file-like object to an internal list as well as a
+    file."""
+    def __init__(self, name, tee=True):
+        """
+        Constructor for CaptureStream. Call redirect() to start redirection.
+
+        Params:
+            name
+                The name of the sys stream to capture (e.g. 'stdout' for
+                sys.stdout)
+            tee
+                If True, forward writing to the original stream after
+                capturing. If False, the redirected stream is not used.
+        """
+        self.softspace = 0
+        self.lines = []
+        self.name = name
+        self.tee = tee
+        self.stream = getattr(sys, name)
+        self.outfile = None
+
+    def write(self, string):
+        """
+        Writes a string to the captured stream.
+
+        Params:
+            string
+                The string to write.
+        """
+        self.lines.append(string)
+        if sys.version_info[0] == 2:
+            self.outfile.write(unicode(string))
+        else:
+            self.outfile.write(string)
+        if self.tee:
+            return self.stream.write(string)
+
+    def flush(self):
+        """Flushes the output file."""
+        self.outfile.flush()
+
+    def hook(self):
+        """Replaces the named stream with the redirected stream."""
+        setattr(sys, self.name, self)
+
+    def unhook(self):
+        """Restores the original stream object."""
+        setattr(sys, self.name, self.stream)
+
+    def redirect(self):
+        """Sets up the initial redirection."""
+        self.outfile = open(
+            paths.get('root', self.name+'.txt'), 'w',
+            encoding='utf-8')
+        self.hook()
+
+def start():
+    """Starts redirection of stdout and stderr."""
+    out.redirect()
+    err.redirect()
+
+out = CaptureStream('stdout', not hasattr(sys, 'frozen'))
+err = CaptureStream('stderr', not hasattr(sys, 'frozen'))
+
+# vim:expandtab
---
+++ core/helpers.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Helper functions."""
+from __future__ import print_function, unicode_literals, absolute_import
+
+import os, glob
+import sys
+from .dfraw import DFRaw
+
+
+def identify_folder_name(base, name):
+    """
+    Allows folder names to be lowercase on case-sensitive systems.
+    Returns "base/name" where name is lowercase if the lower case version
+    exists and the standard case version does not.
+
+    Params:
+        base
+            The path containing the desired folder.
+        name
+            The standard case name of the desired folder.
+    """
+    normal = os.path.join(base, name)
+    lower = os.path.join(base, name.lower())
+    if os.path.isdir(lower) and not os.path.isdir(normal):
+        return lower
+    return normal
+
+def get_text_files(directory):
+    """
+    Returns a list of .txt files in <directory>.
+    Excludes all filenames beginning with "readme" (case-insensitive).
+
+    Params:
+        directory
+            The directory to search.
+    """
+    temp = glob.glob(os.path.join(directory, '*.txt'))
+    result = []
+    for f in temp:
+        if not os.path.basename(f).lower().startswith('readme'):
+            result.append(f)
+    return result
+
+def detect_installed_file(current_file, test_files):
+    """Returns the file in <test_files> which is contained in
+    <current_file>, or "Unknown"."""
+    try:
+        current = DFRaw.read(current_file)
+        for f in test_files:
+            tested = DFRaw.read(f)
+            if tested[-1] == '\n':
+                tested = tested[:-1]
+            if tested in current:
+                return f
+    except IOError:
+        pass
+    return "Unknown"
+
+def detect_installed_files(current_file, test_files):
+    """Returns a list of files in <test_files> that are contained in
+    <current_file>."""
+    installed = []
+    try:
+        current = DFRaw.read(current_file)
+        for f in test_files:
+            try:
+                tested = DFRaw.read(f)
+                if tested[-1] == '\n':
+                    tested = tested[:-1]
+                if tested in current:
+                    installed.append(f)
+            except IOError:
+                pass
+    except IOError:
+        pass
+    return installed
+
+def get_resource(filename):
+    """
+    If running in a bundle, this will point to the place internal
+    resources are located; if running the script directly,
+    no modification takes place.
+    :param str filename:
+    :return str: Path for bundled filename
+    """
+    from .lnp import lnp
+    if lnp.bundle == 'osx':
+        # file is inside application bundle on OS X
+        return os.path.join(os.path.dirname(sys.executable), filename)
+    elif lnp.bundle in ['win', 'linux']:
+        # file is inside executable on Linux and Windows
+        # pylint: disable=protected-access, no-member, maybe-no-member
+        return os.path.join(sys._MEIPASS, filename)
+    else:
+        return os.path.abspath(filename)
