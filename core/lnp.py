b8df114f53dc08f078b929a816ca2396e0b1b360
Dirk Groot <dirk.inge@gmail.com>
2015-02-23 21:54:49 +0100

    Remove directories with os.rmdir in stead of os.remove.

---
+++ core/lnp.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""PyLNP main library."""
+from __future__ import print_function, unicode_literals, absolute_import
+import sys
+
+import os
+from . import errorlog
+
+from .json_config import JSONConfiguration
+
+VERSION = '0.9.3'
+
+lnp = None
+class PyLNP(object):
+    """
+    PyLNP library class.
+
+    Acts as an abstraction layer between the UI and the Dwarf Fortress
+    instance.
+    """
+    def __init__(self):
+        """Constructor for the PyLNP library."""
+        # pylint:disable=global-statement
+        global lnp
+        lnp = self
+        self.BASEDIR = '.'
+        self.bundle = ''
+        if hasattr(sys, 'frozen'):
+            os.chdir(os.path.dirname(sys.executable))
+            if sys.platform == 'win32':
+                self.bundle = 'win'
+            elif sys.platform.startswith('linux'):
+                self.bundle = 'linux'
+            elif sys.platform == 'darwin':
+                self.bundle = 'osx'
+                # OS X bundles start in different directory
+                os.chdir('../../..')
+        else:
+            os.chdir(os.path.join(os.path.dirname(__file__), '..'))
+        self.detect_basedir()
+
+        from . import df, paths, update, utilities
+
+        paths.register('root', self.BASEDIR)
+        errorlog.start()
+
+        paths.register('lnp', self.BASEDIR, 'LNP')
+        if not os.path.isdir(paths.get('lnp')):
+            print('WARNING: LNP folder is missing!', file=sys.stderr)
+        paths.register('keybinds', paths.get('lnp'), 'Keybinds')
+        paths.register('graphics', paths.get('lnp'), 'Graphics')
+        paths.register('utilities', paths.get('lnp'), 'Utilities')
+        paths.register('colors', paths.get('lnp'), 'Colors')
+        paths.register('embarks', paths.get('lnp'), 'Embarks')
+        paths.register('tilesets', paths.get('lnp'), 'Tilesets')
+        paths.register('baselines', paths.get('lnp'), 'Baselines')
+        paths.register('mods', paths.get('lnp'), 'Mods')
+
+        self.df_info = None
+        self.folders = []
+        self.settings = None
+        self.autorun = []
+        self.running = {}
+
+        config_file = 'PyLNP.json'
+        if os.access(paths.get('lnp', 'PyLNP.json'), os.F_OK):
+            config_file = paths.get('lnp', 'PyLNP.json')
+
+        default_config = {
+            "folders": [
+                ["Savegame folder", "<df>/data/save"],
+                ["Utilities folder", "LNP/Utilities"],
+                ["Graphics folder", "LNP/Graphics"],
+                ["-", "-"],
+                ["Main folder", ""],
+                ["LNP folder", "LNP"],
+                ["Dwarf Fortress folder", "<df>"],
+                ["Init folder", "<df>/data/init"]
+            ],
+            "links": [
+                ["DF Homepage", "http://www.bay12games.com/dwarves/"],
+                ["DF Wiki", "http://dwarffortresswiki.org/"],
+                ["DF Forums", "http://www.bay12forums.com/smf/"]
+            ],
+            "hideUtilityPath": False,
+            "hideUtilityExt": False,
+            "updates": {
+                "dffdID": "",
+                "packVersion": "",
+                "checkURL": "",
+                "versionRegex": "",
+                "downloadURL": "",
+                "directURL": ""
+            }
+        }
+        self.config = JSONConfiguration(config_file, default_config)
+        self.userconfig = JSONConfiguration('PyLNP.user')
+
+        df.find_df_folder()
+        utilities.load_autorun()
+
+        self.new_version = None
+
+        from tkgui.tkgui import TkGui
+        self.ui = TkGui()
+        update.check_update()
+        self.ui.start()
+
+    def save_config(self):
+        """Saves LNP configuration."""
+        self.userconfig.save_data()
+
+    def detect_basedir(self):
+        """Detects the location of Dwarf Fortress by walking up the directory
+        tree."""
+        prev_path = '.'
+
+        from . import df
+        try:
+            while os.path.abspath(self.BASEDIR) != prev_path:
+                df.find_df_folders()
+                if len(self.folders) != 0:
+                    break
+                prev_path = os.path.abspath(self.BASEDIR)
+                self.BASEDIR = os.path.join(self.BASEDIR, '..')
+        except UnicodeDecodeError:
+            print(
+                "ERROR: PyLNP is being stored in a path containing non-ASCII "
+                "characters, and cannot continue. Folder names may only use "
+                "the characters A-Z, 0-9, and basic punctuation.\n"
+                "Alternatively, you may run PyLNP from source using Python 3.",
+                file=sys.stderr)
+            sys.exit(1)
+
+
+# vim:expandtab
---
+++ tkgui/binding.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# pylint:disable=invalid-name
+"""Handles control binding for the TKinter GUI."""
+from __future__ import print_function, unicode_literals, absolute_import
+
+import sys
+
+if sys.version_info[0] == 3:  # Alternate import names
+    # pylint:disable=import-error
+    from tkinter import END
+    from tkinter.ttk import Entry
+else:
+    # pylint:disable=import-error
+    from Tkinter import END
+    from ttk import Entry
+
+__controls = dict()
+__lnp = None
+
+def init(lnp):
+    """Connect to an LNP instance."""
+    # pylint:disable=global-statement
+    global __lnp
+    __lnp = lnp
+
+def bind(control, option, update_func=None):
+    """Binds a control to an option."""
+
+    if option not in __controls:
+        __controls[option] = []
+
+    if update_func:
+        value = (control, update_func)
+    else:
+        value = control
+    __controls[option].append(value)
+
+def get(field):
+    """
+    Returns the value of the control known as <field>.
+    If multiple controls are bound, the earliest binding is used.
+    """
+    return __controls[field][0].get()
+
+def update():
+    """Updates configuration displays (buttons, etc.)."""
+    for key in __controls.keys():
+        try:
+            value = getattr(__lnp.settings, key)
+        except KeyError:
+            value = None
+        for entry in __controls[key]:
+            if hasattr(entry, '__iter__'):
+                # Allow (control, func) tuples, etc. to customize value
+                control = entry[0]
+                value = entry[1](value)
+            else:
+                control = entry
+            if isinstance(control, Entry):
+                control.delete(0, END)
+                control.insert(0, value)
+            else:
+                control["text"] = (
+                    control["text"].split(':')[0] + ': ' +
+                    str(value))
+
+# vim:expandtab
