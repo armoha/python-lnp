42c0ec354345e9a19653fbdcd7142d4f7598157d
James Morgensen <james.morgensen@gmail.com>
2015-04-23 00:47:37 -0700

    Move Advanced section to its own tab; most users won't be concerned with it so it shouldn't be immediately visible.

---
+++ core/colors.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Color scheme management."""
+from __future__ import print_function, unicode_literals, absolute_import
+
+import sys, os, shutil
+from . import helpers, paths
+from .lnp import lnp
+from .dfraw import DFRaw
+
+_df_colors = (
+    'BLACK', 'BLUE', 'GREEN', 'CYAN',
+    'RED', 'MAGENTA', 'BROWN', 'LGRAY',
+    'DGRAY', 'LBLUE', 'LGREEN', 'LCYAN',
+    'LRED', 'LMAGENTA', 'YELLOW', 'WHITE'
+)
+
+def read_colors():
+    """Returns a list of color schemes."""
+    return tuple([
+        os.path.splitext(os.path.basename(p))[0] for p in
+        helpers.get_text_files(paths.get('colors'))])
+
+def get_colors(colorscheme=None):
+    """
+    Returns RGB tuples for all 16 colors in <colorscheme>.txt, or
+    data/init/colors.txt if no scheme is provided. On errors, returns an empty
+    list."""
+    # pylint:disable=bare-except
+    try:
+        if colorscheme is not None:
+            f = colorscheme
+            if not f.endswith('.txt'):
+                f = f + '.txt'
+            if os.path.dirname(f) == '':
+                f = paths.get('colors', f)
+        else:
+            if lnp.df_info.version <= '0.31.03':
+                f = paths.get('init', 'init.txt')
+            else:
+                f = paths.get('init', 'colors.txt')
+        color_fields = [(c+'_R', c+'_G', c+'_B') for c in _df_colors]
+        result = DFRaw(f).get_values(*color_fields)
+        return [tuple(int(x) for x in t) for t in result]
+    except:
+        return []
+
+def load_colors(filename):
+    """
+    Replaces the current DF color scheme.
+
+    Params:
+        filename
+        The name of the new colorscheme to install (extension optional).
+        If no path is specified, file is assumed to be in LNP/Colors.
+    """
+    if not filename.endswith('.txt'):
+        filename = filename + '.txt'
+    if os.path.dirname(filename) == '':
+        filename = paths.get('colors', filename)
+    if lnp.df_info.version <= '0.31.03':
+        colors = ([c+'_R' for c in _df_colors] + [c+'_G' for c in _df_colors] +
+                  [c+'_B' for c in _df_colors])
+        lnp.settings.read_file(filename, colors, False)
+        lnp.settings.write_settings()
+    else:
+        shutil.copyfile(filename, paths.get('init', 'colors.txt'))
+
+def save_colors(filename):
+    """
+    Save current keybindings to a file.
+
+    Params:
+        filename
+            The name of the new keybindings file.
+    """
+    if not filename.endswith('.txt'):
+        filename = filename + '.txt'
+    filename = paths.get('colors', filename)
+    if lnp.df_info.version <= '0.31.03':
+        print(
+            "Exporting colors is only supported for DF 0.31.04 and later",
+            file=sys.stderr)
+        colors = ([c+'_R' for c in _df_colors] + [c+'_G' for c in _df_colors] +
+                  [c+'_B' for c in _df_colors])
+        lnp.settings.create_file(filename, colors)
+    else:
+        shutil.copyfile(paths.get('init', 'colors.txt'), filename)
+
+def color_exists(filename):
+    """
+    Returns whether or not a color scheme already exists.
+
+    Params:
+        filename
+            The filename to check.
+    """
+    if not filename.endswith('.txt'):
+        filename = filename + '.txt'
+    return os.access(paths.get('colors', filename), os.F_OK)
+
+def delete_colors(filename):
+    """
+    Deletes a color scheme file.
+
+    Params:
+        filename
+            The filename to delete.
+    """
+    if not filename.endswith('.txt'):
+        filename = filename + '.txt'
+    os.remove(paths.get('colors', filename))
+
+def get_installed_file():
+    """Returns the name of the currently installed color scheme."""
+    files = helpers.get_text_files(paths.get('colors'))
+    current_scheme = get_colors()
+    for scheme in files:
+        if get_colors(scheme) == current_scheme:
+            return os.path.splitext(os.path.basename(scheme))[0]
+    return None
---
+++ tkgui/advanced.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# pylint:disable=unused-wildcard-import,wildcard-import,invalid-name,attribute-defined-outside-init
+"""Advanced tab for the TKinter GUI."""
+from __future__ import print_function, unicode_literals, absolute_import
+
+from . import controls, binding
+from .layout import GridLayouter
+from .tab import Tab
+import sys
+
+from core import launcher, legends_processor
+from core.lnp import lnp
+
+if sys.version_info[0] == 3:  # Alternate import names
+    # pylint:disable=import-error
+    from tkinter import *
+    from tkinter.ttk import *
+    import tkinter.messagebox as messagebox
+else:
+    # pylint:disable=import-error
+    from Tkinter import *
+    from ttk import *
+    import tkMessageBox as messagebox
+
+#pylint: disable=too-many-public-methods
+class AdvancedTab(Tab):
+    """Advanced tab for the TKinter GUI."""
+    def create_variables(self):
+        self.volume_var = StringVar()
+        self.fps_var = StringVar()
+        self.gps_var = StringVar()
+        self.winX_var = StringVar()
+        self.winY_var = StringVar()
+        self.fullX_var = StringVar()
+        self.fullY_var = StringVar()
+
+    def create_controls(self):
+        Grid.columnconfigure(self, 0, weight=1)
+        Grid.columnconfigure(self, 1, weight=1)
+
+        main_grid = GridLayouter(2, pad=(4, 0))
+
+        if lnp.settings.version_has_option('sound'):
+            sound = controls.create_control_group(self, 'Sound')
+            main_grid.add(sound)
+
+            controls.create_option_button(
+                sound, 'Sound', 'Turn game music on/off', 'sound').pack(
+                    side=LEFT, fill=X, expand=Y)
+            if lnp.settings.version_has_option('volume'):
+                controls.create_numeric_entry(
+                    sound, self.volume_var, 'volume',
+                    'Music volume (0 to 255)').pack(side=LEFT, padx=(6, 0))
+                Label(sound, text='/255').pack(side=LEFT)
+        if lnp.settings.version_has_option('fpsCounter'):
+            fps = controls.create_control_group(self, 'FPS')
+            main_grid.add(fps, rowspan=2)
+
+            controls.create_option_button(
+                fps, 'FPS Counter', 'Whether or not to display your FPS',
+                'fpsCounter').pack(fill=BOTH)
+
+            caps = controls.create_control_group(fps, 'FPS Caps')
+            caps.rowconfigure((1, 2), weight=1)
+            caps.columnconfigure((1, 3), weight=1)
+            if lnp.settings.version_has_option('fpsCap'):
+                Label(caps, text='Calculation ').grid(
+                    row=1, column=1, sticky='e')
+                controls.create_numeric_entry(
+                    caps, self.fps_var, 'fpsCap',
+                    'How fast the game runs').grid(
+                        row=1, column=2)
+                Label(caps, text='FPS').grid(row=1, column=3, sticky='w')
+            if lnp.settings.version_has_option('gpsCap'):
+                Label(caps, text='Graphical ').grid(row=2, column=1, sticky='e')
+                controls.create_numeric_entry(
+                    caps, self.gps_var, 'gpsCap', 'How fast the game visually '
+                    'updates.\nLower value may give small boost to FPS but '
+                    'will be less reponsive.').grid(
+                        row=2, column=2, pady=(3, 0))
+                Label(caps, text='FPS').grid(row=2, column=3, sticky='w')
+            if caps.children:
+                caps.pack(fill=BOTH, expand=Y)
+
+        if lnp.settings.version_has_option('introMovie'):
+            startup = controls.create_control_group(self, 'Startup')
+            main_grid.add(startup)
+            Grid.columnconfigure(startup, 0, weight=1)
+
+            controls.create_option_button(
+                startup, 'Intro Movie',
+                'Do you want to see the beautiful ASCII intro movie?',
+                'introMovie').grid(column=0, row=0, sticky="nsew")
+            controls.create_option_button(
+                startup, 'Windowed', 'Start windowed or fullscreen',
+                'startWindowed').grid(column=0, row=1, sticky="nsew")
+
+        resolution = controls.create_control_group(self, 'Resolution')
+        main_grid.add(resolution, 2)
+        resolution['pad'] = (4, 0, 4, 8)
+        resolution.columnconfigure(5, weight=10)
+        resolution.columnconfigure(0, weight=10)
+        resolution.rowconfigure(2, minsize=3)
+
+        Label(resolution, text='Windowed').grid(row=1, column=4, sticky='w')
+        Label(resolution, text='Fullscreen').grid(row=3, column=4, sticky='w')
+        Label(resolution, text='Width').grid(row=0, column=1)
+        Label(resolution, text='Height').grid(row=0, column=3)
+        Label(resolution, text='x').grid(row=1, column=2)
+        Label(resolution, text='x').grid(row=3, column=2)
+        controls.create_numeric_entry(
+            resolution, self.winX_var, ('WINDOWEDX', 'GRAPHICS_WINDOWEDX'),
+            'Horizontal resolution in windowed mode. Values <= 255 represent '
+            'number of tiles, values > 255 represent number of pixels.') \
+            .grid(row=1, column=1)
+        controls.create_numeric_entry(
+            resolution, self.winY_var, ('WINDOWEDY', 'GRAPHICS_WINDOWEDY'),
+            'Vertical resolution in windowed mode. Values <= 255 represent '
+            'number of tiles, values > 255 represent number of pixels.') \
+            .grid(row=1, column=3)
+        controls.create_numeric_entry(
+            resolution, self.fullX_var, ('FULLSCREENX', 'GRAPHICS_FULLSCREENX'),
+            'Horizontal resolution in fullscreen. Values <= 255 represent '
+            'number of tiles, values > 255 represent number of pixels.') \
+            .grid(row=3, column=1)
+        controls.create_numeric_entry(
+            resolution, self.fullY_var, ('FULLSCREENY', 'GRAPHICS_FULLSCREENY'),
+            'Vertical resolution in fullscreen. Values <= 255 represent '
+            'number of tiles, values > 255 represent number of pixels.') \
+            .grid(row=3, column=3)
+
+        saverelated = controls.create_control_group(
+            self, 'Save-related', True)
+        main_grid.add(saverelated, 2)
+
+        grid = GridLayouter(2)
+        grid.add(controls.create_option_button(
+            saverelated, 'Autosave',
+            'How often the game will automatically save', 'autoSave'))
+        grid.add(controls.create_option_button(
+            saverelated, 'Initial Save', 'Saves as soon as you embark',
+            'initialSave'))
+        grid.add(controls.create_option_button(
+            saverelated, 'Pause on Save', 'Pauses the game after auto-saving',
+            'autoSavePause'))
+        grid.add(controls.create_option_button(
+            saverelated, 'Pause on Load', 'Pauses the game as soon as it loads',
+            'pauseOnLoad'))
+        grid.add(controls.create_option_button(
+            saverelated, 'Backup Saves', 'Makes a backup of every autosave',
+            'autoBackup'))
+        if lnp.df_info.version >= '0.31.01':
+            grid.add(controls.create_option_button(
+                saverelated, 'Compress Saves', 'Whether to compress the '
+                'savegames (keep this on unless you experience problems with '
+                'your saves', 'compressSaves'))
+        grid.add(controls.create_trigger_button(
+            saverelated, 'Open Savegame Folder', 'Open the savegame folder',
+            launcher.open_savegames))
+
+        main_grid.add(Frame(self, height=30), 2)
+        main_grid.add(controls.create_option_button(
+            self, 'Processor Priority',
+            'Adjusts the priority given to Dwarf Fortress by your OS',
+            'procPriority'), 2)
+
+        main_grid.add(controls.create_trigger_option_button(
+            self, 'Close GUI on launch',
+            'Whether this GUI should close when Dwarf Fortress is launched',
+            self.toggle_autoclose, 'autoClose', lambda v: ('NO', 'YES')[
+                lnp.userconfig.get_bool('autoClose')]), 2)
+
+        if lnp.df_info.version >= '0.40.09':
+            main_grid.add(controls.create_trigger_button(
+                self, 'Process Legends Exports',
+                'Compress and sort files exported from legends mode',
+                self.process_legends), 2)
+
+    @staticmethod
+    def process_legends():
+        """Process legends exports."""
+        if not legends_processor.get_region_info():
+            messagebox.showinfo('No legends exports',
+                                'There were no legends exports to process.')
+        else:
+            messagebox.showinfo('Exports will be compressed',
+                                'Maps exported from legends mode will be '
+                                'converted to .png format, a compressed archive'
+                                ' will be made, and files will be sorted and '
+                                'moved to a subfolder.  Please wait...')
+            i = legends_processor.process_legends()
+            string = str(i) + ' region'
+            if i > 1:
+                string += 's'
+            messagebox.showinfo(string + ' processed',
+                                'Legends exported from ' + string +
+                                ' were found and processed')
+
+    @staticmethod
+    def toggle_autoclose():
+        """Toggle automatic closing of the UI when launching DF."""
+        launcher.toggle_autoclose()
+        binding.update()
+
